9aaf52da3e7d7155748cbb90d21942ba0c9ba85d
efont25 <79472270+efont25@users.noreply.github.com>
2025-03-11 12:50:21 +0000

    Refactor page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
 
 import "./globals.css";
+import { FloatingNav } from "./components/ui/floating-navbar";
+import Footer from "./components/Footer";
 
 const fontBrandRegular = localFont({
   src: "./fonts/borna-regular-webfont.woff2",
@@ -18,7 +21,28 @@ export const metadata: Metadata = {
   title: "PKS Solutions Website",
   description: "Welcome to PKS Solutions, your partner in innovation.",
 };
-
+const navItems = [
+  {
+    name: "Home",
+    link: "/",
+    icon: <IconHome className="w-4 h-4 text-neutral-500 dark:text-white" />,
+  },
+  {
+    name: "Lösungen",
+    link: "/solutions",
+    icon: <IconUser className="w-4 h-4 text-neutral-500 dark:text-white" />,
+  },
+  {
+    name: "Über uns",
+    link: "/about",
+    icon: <IconUser className="w-4 h-4 text-neutral-500 dark:text-white" />,
+  },
+  {
+    name: "Kontakt",
+    link: "/contact",
+    icon: <IconMessage className="w-4 h-4 text-neutral-500 dark:text-white" />,
+  },
+];
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -27,7 +51,13 @@ export default function RootLayout({
   return (
     <html lang="de">
       <body className={` ${fontBrandRegular.className} antialiased`}>
-        {children}
+        <div className="justify-items-center items-center grid grid-rows-[20px_1fr_10px] p-0 w-full">
+          <main className="items-center sm:items-start gap-8 row-start-2 bg-black w-full">
+            <FloatingNav navItems={navItems} />
+            {children}
+            <Footer />
+          </main>
+        </div>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
-import Footer from "./components/Footer";
-import { FloatingNav } from "./components/ui/floating-navbar";
 import { LampDemo } from "./components/ui/lamp";
 import { PageType } from "@/types/types";
 import { getPageBySlug } from "@/sanity/fetchData";
@@ -9,50 +6,23 @@ export default async function Home() {
   const page: PageType = await getPageBySlug("home");
   //console.log(page);
   const { title, content } = page;
-  const navItems = [
-    {
-      name: "Home",
-      link: "/",
-      icon: <IconHome className="w-4 h-4 text-neutral-500 dark:text-white" />,
-    },
-    {
-      name: "Lösungen",
-      link: "/solutions",
-      icon: <IconUser className="w-4 h-4 text-neutral-500 dark:text-white" />,
-    },
-    {
-      name: "Über uns",
-      link: "/about",
-      icon: <IconUser className="w-4 h-4 text-neutral-500 dark:text-white" />,
-    },
-    {
-      name: "Kontakt",
-      link: "/contact",
-      icon: (
-        <IconMessage className="w-4 h-4 text-neutral-500 dark:text-white" />
-      ),
-    },
-  ];
+
   return (
-    <div className="justify-items-center items-center grid grid-rows-[20px_1fr_10px] p-0 w-full">
-      <FloatingNav navItems={navItems} />
-      <main className="items-center sm:items-start gap-8 row-start-2 bg-black w-full">
-        {content.map((block, index) => {
-          //console.log(block);
-          switch (block._type) {
-            case "hero":
-              return <HeroHighlightComponent key={index} {...block} />;
-            default:
-              return null;
-          }
-        })}
-        {/* <TabsDemo className="relative z-30 flex justify-center items-center gap-4 w-full h-[40rem]" /> */}
-        <div className="flex flex-col justify-center items-center w-full">
-          <LampDemo />{" "}
-        </div>{" "}
-        {/* <ParallaxScroll images={images} />;  */}
-        <Footer />
-      </main>
-    </div>
+    <>
+      {content.map((block, index) => {
+        //console.log(block);
+        switch (block._type) {
+          case "hero":
+            return <HeroHighlightComponent key={index} {...block} />;
+          default:
+            return null;
+        }
+      })}
+      {/* <TabsDemo className="relative z-30 flex justify-center items-center gap-4 w-full h-[40rem]" /> */}
+      <div className="flex flex-col justify-center items-center w-full">
+        <LampDemo />{" "}
+      </div>{" "}
+      {/* <ParallaxScroll images={images} />;  */}
+    </>
   );
 }
