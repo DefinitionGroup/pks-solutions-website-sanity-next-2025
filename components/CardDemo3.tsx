e09319b087b514a100a2b5414e596d6e9653d9c9
mainframeai <martin@definition.studio>
2026-02-03 12:09:55 +0100

    feat: `CardDemo3` now conditionally renders video or image assets, updating prop usage in `GridHero3`.

--- components/CardDemo3.tsx
+++ components/CardDemo3.tsx
@@ -1,13 +1,17 @@
 "use client";
 import { cn } from "@/app/lib/utils";
 import { CloudinaryAsset } from "@/types/types";
+import Image from "next/image";
+
 interface CardDemo3Props {
   title: string;
   subtitle: string;
-  video: CloudinaryAsset;
+  media: CloudinaryAsset;
 }
 
-export function CardDemo3({ title, subtitle, video }: CardDemo3Props) {
+export function CardDemo3({ title, subtitle, media }: CardDemo3Props) {
+  const isVideo = media.resource_type === "video";
+
   return (
     <div className="w-full h-full min-h-full">
       <div
@@ -15,16 +19,24 @@ export function CardDemo3({ title, subtitle, video }: CardDemo3Props) {
           "group w-full min-h-full bg-black cursor-pointer min-h-[24rem] overflow-hidden relative card h-full shadow-xl mx-auto flex flex-col justify-end p-4  dark:border-neutral-800"
         )}
       >
-        {" "}
-        <video
-          autoPlay
-          loop
-          muted
-          className="bottom-0 absolute border  object-cover opacity-70 mt-8 w-full h-full min-h-full scale-125"
-        >
-          <source src={video.secure_url} type="video/mp4" /> Your browser does
-          not support the video tag.
-        </video>
+        {isVideo ? (
+          <video
+            autoPlay
+            loop
+            muted
+            className="bottom-0 absolute border object-cover opacity-70 mt-8 w-full h-full min-h-full scale-125"
+          >
+            <source src={media.secure_url} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        ) : (
+          <Image
+            src={media.secure_url}
+            alt={title}
+            fill
+            className="bottom-0 absolute border object-cover opacity-70 mt-8 scale-125"
+          />
+        )}
         <div className="top-0 z-50 absolute pt-12 text">
           <h3 className="relative font-bold text-gray-50 text-xl pr-24 md:text-3xl">
             {title}
